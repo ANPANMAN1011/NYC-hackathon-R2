5fba37695d212e5a127b631cb9c7003590e9283c
Harshill <Harshillarora376@gmail.com>
2026-07-22 13:00:57 +0530

    commit a

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 // function for start button
-let startButton = document.querySelector(".start-btn");
+const startButton = document.querySelector(".start-btn");
+
+// implement code for switching screens
+const startScreen = document.querySelector(".start-screen");
+const Screen1 = document.querySelector(".screen1");
+
+// transistion to first qurestion
+function goToFirstQuestion(){
+    // hide the start screen
+    startScreen.classList.add("hidden");
+
+    // show the Screen1 here
+    Screen1.classList.remove("hidden");
+}
+
+// adding event listner to the strt button
+if (startButton){
+    startButton.addEventListener("click", goToFirstQuestion);
+}
